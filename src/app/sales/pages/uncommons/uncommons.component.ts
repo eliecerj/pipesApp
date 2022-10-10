f9848b26311a16b020274d25554f149ef92fd371
Eliecer Arteaga <earteaga@sura.cl>
2022-10-10 14:30:17 -0300

    creacion de componentes menu, basics, uncommons, order y creacion de modulo sales y approuter

---
+++ src/app/sales/pages/uncommons/uncommons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uncommons',
+  templateUrl: './uncommons.component.html',
+  styleUrls: ['./uncommons.component.css']
+})
+export class UncommonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/sales/sales.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NumbersComponent } from './pages/numbers/numbers.component';
+import { UncommonsComponent } from './pages/uncommons/uncommons.component';
+import { BasicsComponent } from './pages/basics/basics.component';
+import { OrderComponent } from './pages/order/order.component';
+
+
+
+@NgModule({
+  declarations: [
+    NumbersComponent,
+    UncommonsComponent,
+    BasicsComponent,
+    OrderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SalesModule { }
