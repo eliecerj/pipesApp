d81908fff61263f3a2906aae8adb7144f793b055
Eliecer Arteaga <earteaga@sura.cl>
2022-10-12 00:17:40 -0300

    DecimalPipe

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
   styleUrls: ['./numbers.component.css']
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  netSales: number = 2567789.5567;
+  percentage: number = 0.48;
 
 }
