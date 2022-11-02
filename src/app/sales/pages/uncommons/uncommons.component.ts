7009e39f0c9adaef589ad5311a7aa57e5bf0e6b8
Eliecer Arteaga <eliecerj@gmail.com>
2022-11-02 00:50:56 -0300

    i18nPural Pipe ejemplo sencillo

--- src/app/sales/pages/uncommons/uncommons.component.ts
+++ src/app/sales/pages/uncommons/uncommons.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UncommonsComponent implements OnInit {
 
+  // i18nSelect
   name: string = 'Eliecer';
   genre: string = 'male';
 
   invitationMap = {
     'male': 'invitarlo',
     'female': 'invitarla'
+  }
+
+  // 18nPlural
+  clients: string[] = ['Maria', 'Pedro', 'Sanet', 'Rosio'];
+  clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando'
   }
 
   constructor() { }
